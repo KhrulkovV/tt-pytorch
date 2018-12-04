c5a04567b223469693ec659d4e03904f6cced175
geom-score <vk295@cornell.edu>
2018-12-04 16:18:00 +0300

    gpu fix

--- tttorch/ops.py
+++ tttorch/ops.py
@@ -1,18 +1,17 @@
 from tttorch import TensorTrainBatch
 import torch
-import numpy as np
 
 
-def gather_rows(mat, inds):
+def gather_rows(tt_mat, inds):
     """
     inds -- list of indices of shape batch_size x d
     d = len(mat.raw_shape[1])
     """
-    cores = mat.tt_cores
+    cores = tt_mat.tt_cores
     slices = []
     for k, core in enumerate(cores):
         i = inds[:, k]
         core = core.permute(1, 0, 2, 3)
         slices.append(torch.index_select(core, 0, i))
 
-    return TensorTrainBatch(slices)
+    return TensorTrainBatch(slices, convert_to_tensors=False)
