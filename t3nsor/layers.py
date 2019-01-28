30cdbea4801afcdc5043ce38c1d07159a024ca92
tt-embedding <email@example.com>
2019-01-28 18:24:07 +0300

    better auto shape

--- t3nsor/layers.py
+++ t3nsor/layers.py
@@ -5,27 +5,30 @@ import t3nsor as t3
 
 
 class TTEmbedding(nn.Module):
-    def __init__(self, 
-                 init=None, 
-                 shape=None, 
+    def __init__(self,
+                 init=None,
+                 shape=None,
                  voc_size=None,
                  emb_size=None,
                  auto_shapes=None,
+                 auto_shape_mode='ascending',
+                 auto_shape_criterion='entropy',
                  d=3,
-                 tt_rank=8, 
+                 tt_rank=8,
                  stddev=None,
-                 batch_dim_last=None, 
+                 batch_dim_last=None,
                  padding_idx=None):
-        
+
         super(TTEmbedding, self).__init__()
 
         if auto_shapes:
-            voc_quantization =  t3.utils.suggest_tt(voc_size, d=d)
-            emb_quantization = t3.utils.get_tt_shape(emb_size, d=d)
-            
+            voc_quantization = t3.utils.suggest_shape(
+                voc_size, d=d, criterion=auto_shape_criterion, mode=auto_shape_mode)
+            emb_quantization = t3.utils.auto_shape(
+                emb_size, d=d, criterion=auto_shape_criterion, mode=auto_shape_mode)
+
             shape = [voc_quantization, emb_quantization]
-            
-        
+
         if init is None:
             if shape is None:
                 raise ValueError("if init is not provided, please specify shape")
@@ -39,82 +42,84 @@ class TTEmbedding(nn.Module):
 
         self.tt_matrix = init.to_parameter()
         self.parameters = self.tt_matrix.parameter
-        
-        #for p in self.parameters():
+
+        # for p in self.parameters():
         #    p.name = 'tt_core'
-        
+
         self.batch_dim_last = batch_dim_last
         self.voc_size = int(np.prod(self.shape[0]))
         self.emb_size = int(np.prod(self.shape[1]))
-        
+
         self.voc_quant = self.shape[0]
         self.emb_quant = self.shape[1]
-        
+
         self.padding_idx = padding_idx
 
     def forward(self, x):
-        
+
         xshape = list(x.shape)
-        xshape_new = xshape + [self.emb_size,]
+        xshape_new = xshape + [self.emb_size, ]
         x = x.view(-1)
-          
+
         # x_ind = t3.ind2sub(self.voc_quant, x)
         # rows = t3.gather_rows(self.tt_matrix, x_ind)
-                 
+
         # rows = rows.view(x.shape[0], -1)
-        
+
         full = self.tt_matrix.full()
         rows = full[x]
-        
+
         if self.padding_idx is not None:
             rows = torch.where(x.view(-1, 1) != self.padding_idx, rows, torch.zeros_like(rows))
-                 
-        
+
         rows = rows.view(*xshape_new)
-        
+
         return rows.to(x.device)
-    
-    
+
+
 class TTLinear(nn.Module):
     def __init__(self, in_features=None, out_features=None, bias=True, init=None, shape=None,
-                 auto_shapes=True, d=3, tt_rank=8, stddev=None 
-                ):
+                 auto_shapes=True, d=3, tt_rank=8, stddev=None, auto_shape_mode='ascending',
+                 auto_shape_criterion='entropy',
+                 ):
         super(TTLinear, self).__init__()
-        
+
         if auto_shapes:
             if in_features is None or out_features is None:
                 raise ValueError("Shape is not specified")
-            
-            in_quantization =  t3.utils.get_tt_shape(in_features, d=d)
-            out_quantization = t3.utils.get_tt_shape(out_features, d=d)
-            
+
+            in_quantization = t3.utils.auto_shape(
+                in_features, d=d, criterion=auto_shape_criterion, mode=auto_shape_mode)
+            out_quantization = t3.utils.auto_shape(
+                out_features, d=d, criterion=auto_shape_criterion, mode=auto_shape_mode)
+
             shape = [in_quantization, out_quantization]
-        
+
         if init is None:
             if shape is None:
-                raise ValueError("if init is not provided, please specify shape, or set auto_shapes=True")
+                raise ValueError(
+                    "if init is not provided, please specify shape, or set auto_shapes=True")
         else:
             shape = init.raw_shape
 
         if init is None:
-            init = t3.glorot_initializer(shape, tt_rank=tt_rank)     
-            
+            init = t3.glorot_initializer(shape, tt_rank=tt_rank)
+
         self.shape = shape
         self.weight = init.to_parameter()
         self.parameters = self.weight.parameter
         self.weight_t = t3.transpose(self.weight)
-        
+
         if bias:
             self.bias = torch.nn.Parameter(1e-2 * torch.ones(out_features))
         else:
             self.register_parameter('bias', None)
 
-
     def forward(self, x):
         weight_t = self.weight_t
         x_t = x.transpose(0, 1)
-    
-        if self.bias is None: 
-            return t3.tt_dense_matmul(weight_t, x_t).transpose(0, 1) 
+
+        if self.bias is None:
+            return t3.tt_dense_matmul(weight_t, x_t).transpose(0, 1)
         else:
             return t3.tt_dense_matmul(weight_t, x_t).transpose(0, 1) + self.bias
--- t3nsor/utils.py
+++ t3nsor/utils.py
@@ -1,162 +1,95 @@
-import numpy as np
-import torch
-from itertools import chain, combinations, product
 from scipy.stats import entropy
 import numpy as np
 from sympy.utilities.iterables import multiset_partitions
-import math
+from sympy.ntheory import factorint
+from itertools import cycle, islice
 
-def _primes(n, prime_numbers = []):
-    """ Returns  a list of primes <= n """
-    if len(prime_numbers) > 0:
-        if prime_numbers[-1] >= n:
-            return [p for p in prime_numbers if p <= n]
-    else:
-        return _generate_primes(n + 1, prime_numbers)
-
-def _limit(n):
-    return int(math.ceil(math.sqrt(n)))
-
-def _generate_primes(n, prime_numbers = []):
-    start = 3
-
-    if len(prime_numbers) > 0:
-        sieve = [False] * prime_numbers[-1]
-
-        start = len(sieve)
-
-        while len(sieve) < n:
-            sieve.append(True)
-
-        for p in prime_numbers:
-            if p < n:
-                sieve[p] = True
-    else:
-        sieve = [True] * n
-
-    for i in range(start, _limit(n), 2):
-        if sieve[i]:
-            sieve[i * i :: 2 * i] = [False] * int((n - i * i - 1) / (2 * i) + 1)
-
-    return [2] + [i for i in range(3, n, 2) if sieve[i]]
-
-def _is_prime(n, prime_numbers = []):
-    """ Returns true if n is prime; otherwise false """
-    if n < 2:
-        return False
-
-    if len(prime_numbers) == 0 or prime_numbers[-1] < n:
-        prime_numbers = _generate_primes(n + 1, prime_numbers)
-
-    for p in prime_numbers:
-        if n < p:
-            return False
-        if n == p:
-            return True
-
-    return False
-
-def _factor(n, prime_numbers = []):
-    """ Returns list of prime factors of a integer """
-    factors = []
-
-    if not isinstance(n, int):
-        return factors
-
-    n = abs(n)
-
-    if n == 0 or n == 1:
-        factors.append(n)
-    else:
-        limit = _limit(n)
-
-        if len(prime_numbers) == 0 or prime_numbers[-1] < limit:
-            prime_numbers = _generate_primes(limit + 1, prime_numbers)
-
-        for p in prime_numbers:
-            while True:
-                if n % p == 0:
-                    factors.append(p)
-
-                    n = n // p
-                else:
-                    break
-
-            if p > n:
-                break
-
-        if n != 1:
-            factors.append(n)
-
-        if len(factors) == 0:
-            factors.append(n)
-
-    return factors
+MODES = ['ascending', 'descending', 'mixed']
+CRITERIONS = ['entropy', 'var']
 
 
-
-def ind2sub(siz, idx):
-    n = len(siz)
-    b = len(idx)
-    subs = []
-    k = np.cumprod(siz[:-1])
-    k = np.concatenate((np.ones(1), k))
-
-    for i in range(n - 1, -1, -1):
-        subs.append(torch.floor(idx.float() / k[i]).long())
-        idx = torch.fmod(idx, k[i])
-
-    return torch.stack(subs[::-1], dim=1)
+def _to_list(p):
+    res = []
+    for k, v in p.items():
+        res += [k, ] * v
+    return res
 
 
-def svd_fix(x):
-    n = x.shape[0]
-    m = x.shape[1]
-    
-    if n > m:
-        u, s, v = torch.svd(x)
-        
-    else:
-        u, s, v = torch.svd(x.t())
-        v, u = u, v
-    
-    return u, s, v
-
-def _get_all_factors(n, d=3):
-    
-    p = _factor(n)
-    if len(p) < d:
-        p = p + [1,] * (d - len(p))
-    raw_factors = multiset_partitions(p, d)
-    clean_factors = [tuple(sorted(np.prod(_) for _ in f)) for f in raw_factors]
-    clean_factors = list(set(clean_factors))
-    return(clean_factors)
-    
-    
-def get_tt_shape(n, d=3):
-    factors = _get_all_factors(n, d)
-    if len(factors) == 0:
-        raise ValueError('No possible factorizations, try lower d')
-    entropies = [entropy(f) for f in factors]
-    i = np.argmax(entropies)
-    
-    return list(factors[i])
-    
-    
 def _roundup(n, k):
     return int(np.ceil(n / 10**k)) * 10**k
 
-def suggest_tt(n, d=3):
-    
-    e = []
-    
+
+def _roundrobin(*iterables):
+    "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
+    # Recipe credited to George Sakkis
+    pending = len(iterables)
+    nexts = cycle(iter(it).__next__ for it in iterables)
+    while pending:
+        try:
+            for next in nexts:
+                yield next()
+        except StopIteration:
+            pending -= 1
+            nexts = cycle(islice(nexts, pending))
+
+
+def _get_all_factors(n, d=3, mode='ascending'):
+    p = _factorint2(n)
+    if len(p) < d:
+        p = p + [1, ] * (d - len(p))
+
+    if mode == 'ascending':
+        def prepr(x):
+            return tuple(sorted([np.prod(_) for _ in x]))
+    elif mode == 'descending':
+        def prepr(x):
+            return tuple(sorted([np.prod(_) for _ in x], reverse=True))
+
+    elif mode == 'mixed':
+        def prepr(x):
+            x = sorted(np.prod(_) for _ in x)
+            N = len(x)
+            xf, xl = x[:N//2], x[N//2:]
+            return tuple(_roundrobin(xf, xl))
+
+    else:
+        raise ValueError('Wrong mode specified, only {} are available'.format(MODES))
+
+    raw_factors = multiset_partitions(p, d)
+    clean_factors = [prepr(f) for f in raw_factors]
+    clean_factors = list(set(clean_factors))
+    return clean_factors
+
+
+def _factorint2(p):
+    return _to_list(factorint(p))
+
+
+def auto_shape(n, d=3, criterion='entropy', mode='ascending'):
+    factors = _get_all_factors(n, d=d, mode=mode)
+    if criterion == 'entropy':
+        weights = [entropy(f) for f in factors]
+    elif criterion == 'var':
+        weights = [-np.var(f) for f in factors]
+    else:
+        raise ValueError('Wrong criterion specified, only {} are available'.format(CRITERIONS))
+
+    i = np.argmax(weights)
+    return list(factors[i])
+
+
+def suggest_shape(n, d=3, criterion='entropy', mode='ascending'):
+    weights = []
     for i in range(len(str(n))):
-        e.append(entropy(get_tt_shape(_roundup(n, i), d=d)))
 
-    i = np.argmax(np.array(e))
-   
-    factors = get_tt_shape(int(_roundup(n, i)), d=d)
+        n_i = _roundup(n, i)
+        if criterion == 'entropy':
+            weights.append(entropy(auto_shape(n_i, d=d, mode=mode, criterion=criterion)))
+        elif criterion == 'var':
+            weights.append(-np.var(auto_shape(n_i, d=d, mode=mode, criterion=criterion)))
+        else:
+            raise ValueError('Wrong criterion specified, only {} are available'.format(CRITERIONS))
 
-    # print('n = %d | suggested voc size: %d with factors %a' % (n, roundup(n, i), factors))
-
+    i = np.argmax(weights)
+    factors = auto_shape(int(_roundup(n, i)), d=d, mode=mode, criterion=criterion)
     return factors
